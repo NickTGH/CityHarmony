acf88bd8cec6b5739ada9d961a635c5ea5dda2ec
NickTGH <nik123424@abv.bg>
2024-01-16 10:58:39 +0200

    Finished Building system
    finished the placement and added removal of buildings

--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -6,17 +6,107 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mouseIndicator, cellIndicator;
-    [SerializeField]
     private InputManager inputManager;
     [SerializeField]
     private Grid grid;
 
-    private void Update()
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private GridData floorData, structureData;
+
+    [SerializeField]
+    private PreviewSystem preview;
+
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
+    [SerializeField]
+    private ObjectPlacer objectPlacer;
+
+    IBuildingState buildingState;
+
+    private void Start()
     {
+        StopPlacement();
+        floorData = new();
+        structureData = new();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        gridVisualization.SetActive(true);
+        buildingState = new PlacementState(ID,
+                                           grid,
+                                           preview,
+                                           database,
+                                           floorData,
+                                           structureData,
+                                           objectPlacer);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    public void StartRemoving()
+    {
+        StopPlacement();
+        gridVisualization.SetActive(true);
+        buildingState = new RemovingState(grid,
+                                          preview,
+                                          floorData,
+                                          structureData,
+                                          objectPlacer);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+
         Vector2 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
-        mouseIndicator.transform.position = mousePosition;
-        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+
+        buildingState.OnAction(gridPosition);
+    }
+
+    //private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    //{
+    //    GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData : structureData;
+
+    //    return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
+    //}
+
+    public void StopPlacement()
+    {
+        if (buildingState == null)
+            return;
+        gridVisualization.SetActive(false);
+        buildingState.EndState();
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+        lastDetectedPosition = Vector3Int.zero;
+        buildingState = null;
+    }
+    private void Update()
+    {
+       if(buildingState==null)
+       {
+           return;
+       }
+        Vector2 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        if(lastDetectedPosition != gridPosition)
+        {
+            buildingState.UpdateState(gridPosition);
+            lastDetectedPosition = gridPosition;
+        }
     }
 }
